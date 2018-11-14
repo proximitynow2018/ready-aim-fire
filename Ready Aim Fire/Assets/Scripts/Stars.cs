9de2de748fa8bc75ef278c535b22e8c83dbc0956
proximitynow2018 <44660823+proximitynow2018@users.noreply.github.com>
2018-11-14 17:37:12 +1300

    Hopefully added star system

---
+++ Ready Aim Fire/Assets/Scripts/Stars.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Stars : MonoBehaviour {
+
+	[SerializeField]
+	private int Occurance = 10;
+	[SerializeField]
+	private GameObject Star;
+	[SerializeField]
+	private int Radius = 5;
+	[SerializeField]
+	private Vector3 Offset;
+
+	private int Temp_RandomInt;
+	private GameObject Temp_Clone;
+
+	private void Start()
+	{
+		Debug.Log("Drawing Stars");
+		for (int i = 0; (Occurance * 10) < i; i++)
+		{
+			Temp_RandomInt = Random.Range(0, Occurance + 1);
+			if (Temp_RandomInt == 1)
+			{
+				Temp_Clone = Instantiate(Star, transform);
+				Temp_Clone.transform.position = new Vector3(Random.Range(-1, Radius + 1), Random.Range(-1, Radius + 1), 1) + Offset;
+			}
+		}
+		Debug.Log("Stars Drawn");
+	}
+}
